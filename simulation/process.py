f33490ddb5a7555d2f7d58931659f45382b2634c
Smita Vijaya Kumar <sv440@user-109-116.vpn.cl.cam.ac.uk>
2020-09-17 10:00:29 +0530

    Cleaning up stale results

--- simulation/process.py
+++ simulation/process.py
@@ -34,6 +34,7 @@
 
 import sys
 import numpy as np
+import operator
 
 if(len(sys.argv) != 6): 
     print "Incorrent number of parameters."
@@ -43,30 +44,35 @@ if(len(sys.argv) != 6):
 # pypy process.py finished_file eagle short yahoo 4000 
 infile = open(sys.argv[1], 'r')
 outfile = open(sys.argv[2].lower()+"_"+sys.argv[3]+"_"+sys.argv[4], 'a+')
-timesfile = open(sys.argv[2] +"_"+sys.argv[3] + "_time_"+sys.argv[4], 'w')
 jobrunningtime = []
-jobid_runningtime = {}
+schedulertime = []
+waittime = []
+processingtime = []
 for line in infile:
     job_short_long = "short" if ('by_def:  0' in line) else "long"
     #This is the type of job we are looking for
     if job_short_long != sys.argv[3]:
         continue
-    # Job id is the last parameter, so everything after it describes the ID
-    jobid = int(line.split('job id ')[1])
-    runningtime = float(line.split('total_job_running_time: ')[1].split(' ')[1])
+    runningtime = float((line.split('total_job_running_time: ')[1]).split()[0])
+    jobid = int(((line.split('job_id ')[1]).split())[0])
+    
+    scheduler_time = float(((line.split('scheduler_algorithm_time ')[1]).split())[0])
+    task_wait_time = float(((line.split('task_wait_time ')[1]).split())[0])
+    processing_time = float(((line.split('task_processing_time ')[1]).split())[0])
+
     jobrunningtime.append(runningtime)
-    jobid_runningtime[jobid] = runningtime
-    timesfile.write("%s\t%s\n"%(jobid, jobid_runningtime[jobid] ))
+    schedulertime.append(scheduler_time)
+    waittime.append(task_wait_time)
+    processingtime.append(processing_time)
 
 infile.close()
 
 jobrunningtime.sort()
-#print jobrunningtime
-#print np.percentile(jobrunningtime, 50)
-#print np.percentile(jobrunningtime, 90)
-#print np.percentile(jobrunningtime, 99)
-outfile.write("%s\t%s\n"% ("Cluster Size ", sys.argv[5]))
-outfile.write("%s\t%s\n"% ("50th percentile: ",  np.percentile(jobrunningtime, 50))) 
-outfile.write("%s\t%s\n" % ("90th percentile: ", np.percentile(jobrunningtime, 90))) 
-outfile.write("%s\t%s\n" % ("99th percentile: ", np.percentile(jobrunningtime, 99))) 
+schedulertime.sort()
+waittime.sort()
+processingtime.sort()
+outfile.write("%s\t%s\t(%s\t%s\t%s)\n"% ("Cluster Size ", sys.argv[5], "(Scheduler Time", "Task Wait Time", "Task Processing Time)"))
+outfile.write("%s\t%s\t(%s\t%s\t%s)\n"% ("50th percentile: ",  np.percentile(jobrunningtime, 50), np.percentile(schedulertime, 50), np.percentile(waittime, 50), np.percentile(processingtime, 50))) 
+outfile.write("%s\t%s\t(%s\t%s\t%s)\n" % ("90th percentile: ", np.percentile(jobrunningtime, 90), np.percentile(schedulertime, 90), np.percentile(waittime, 90), np.percentile(processingtime, 90))) 
+outfile.write("%s\t%s\t(%s\t%s\t%s)\n" % ("99th percentile: ", np.percentile(jobrunningtime, 99), np.percentile(schedulertime, 99), np.percentile(waittime, 99), np.percentile(processingtime, 99))) 
 outfile.close()
