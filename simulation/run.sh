3e3d51efcaecdee8ff3bdc4562a58215e5fe6324
Smita Vijaya Kumar <sv440@user-109-116.vpn.cl.cam.ac.uk>
2020-09-23 17:26:44 +0530

    Reverting f12ada46cb8484a86d969f2e7dcf761146acdd77 which gives bad numbers for short jobs

--- simulation/run.sh
+++ simulation/run.sh
@@ -1,16 +1,28 @@
 ################## YAHOO TRACES ###########################
-# Hawk
-pypy simulation_main.py YH.tr yes yes 90.5811 90.5811 100 98 1 2 5 MEAN 0 0 ATC 10000 10 8000 no 0 2 no Hawk
 # Sparrow
-pypy simulation_main.py YH.tr no no 90.5811 90.5811 100 100 1 2 5 MEAN 0 0 ATC 10000 10 8000 no 0 2 no Hawk
+pypy simulation_main.py YH.tr no no 90.5811 90.5811 100 100 1 2 5 MEAN 0 0 ATC 10000 10 1000 no 0 2 no Hawk no 0 0
 # Eagle
-pypy simulation_main.py YH.tr no yes 90.5811 90.5811 100 98 1 2 5 MEAN 0 0 ATC 10000 10 8000 yes 0 20 yes Eagle
-# Ideal Eagle
-pypy simulation_main.py YH.tr no yes 90.5811 90.5811 100 98 1 2 5 MEAN 0 0 ATC 10000 10 8000 no 0 20 yes IdealEagle
-# LWL with Partitioning
-pypy simulation_main.py YH.tr no yes -1 90.5811 100 98 1 2 5 MEAN 0 0 ATC 10000 10 8000 no 0 2 no CLWL
-# DLWL with Partitioning and SRPT
-pypy simulation_main.py YH.tr no yes -1 90.5811 100 98 1 2 5 MEAN 0 0 ATC 10000 10 8000 yes 8 2 no DLWL
-# Murmuration with resource heartbeats generated as required, and all jobs
-# considered long, and scheduled in a centralized way 
-pypy simulation_main.py YH.tr no yes -1 90.5811 100 100 1 2 5 MEAN 0 0 ATC 10000 10 8000 yes 0 2 no Murmuration
+pypy simulation_main.py YH.tr no yes 90.5811 90.5811 100 98 1 2 5 MEAN 0 0 ATC 10000 10 1000 yes 0 20 yes Eagle no 0 0
+# Murmuration
+pypy simulation_main.py YH.tr no yes 90.5811 90.5811 100 100 1 2 5 MEAN 0 0 ATC 10000 10 1000 no 0 2 no Murmuration no 10 10000
+
+# Sparrow
+pypy simulation_main.py YH.tr no no 90.5811 90.5811 100 100 1 2 5 MEAN 0 0 ATC 10000 10 2000 no 0 2 no Hawk no 0 0
+# Eagle
+pypy simulation_main.py YH.tr no yes 90.5811 90.5811 100 98 1 2 5 MEAN 0 0 ATC 10000 10 2000 yes 0 20 yes Eagle no 0 0
+# Murmuration
+pypy simulation_main.py YH.tr no yes 90.5811 90.5811 100 100 1 2 5 MEAN 0 0 ATC 10000 10 2000 no 0 2 no Murmuration no 10 10000
+
+# Sparrow
+pypy simulation_main.py YH.tr no no 90.5811 90.5811 100 100 1 2 5 MEAN 0 0 ATC 10000 10 4000 no 0 2 no Hawk no 0 0
+# Eagle
+pypy simulation_main.py YH.tr no yes 90.5811 90.5811 100 98 1 2 5 MEAN 0 0 ATC 10000 10 4000 yes 0 20 yes Eagle no 0 0
+# Murmuration
+pypy simulation_main.py YH.tr no yes 90.5811 90.5811 100 100 1 2 5 MEAN 0 0 ATC 10000 10 4000 no 0 2 no Murmuration no 10 10000
+
+# Sparrow
+pypy simulation_main.py YH.tr no no 90.5811 90.5811 100 100 1 2 5 MEAN 0 0 ATC 10000 10 8000 no 0 2 no Hawk no 0 0
+# Eagle
+pypy simulation_main.py YH.tr no yes 90.5811 90.5811 100 98 1 2 5 MEAN 0 0 ATC 10000 10 8000 yes 0 20 yes Eagle no 0 0
+# Murmuration
+pypy simulation_main.py YH.tr no yes 90.5811 90.5811 100 100 1 2 5 MEAN 0 0 ATC 10000 10 8000 no 0 2 no Murmuration no 10 10000
